07a91e3fa5ebb39bad4f92285b54cf5770937dba
CSeungJoo <seungjoo13579@gmail.com>
2024-06-10 21:18:25 +0900

    init

---
+++ src/main/kotlin/kr/pah/pcs/pcsoauth/PcsOauthApplication.kt
@@ -0,0 +1,11 @@
+package kr.pah.pcs.pcsoauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PcsOauthApplication
+
+fun main(args: Array<String>) {
+    runApplication<PcsOauthApplication>(*args)
+}
---
+++ src/test/kotlin/kr/pah/pcs/pcsoauth/PcsOauthApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.pah.pcs.pcsoauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PcsOauthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
